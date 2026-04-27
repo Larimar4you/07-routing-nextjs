e39459a6bfa579f87f9dfc643817bc300096e270
Larimar4you <Zajanezlaja@gmail.com>
2026-04-27 19:42:25 +0200

    GO

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -33,6 +33,7 @@ export default function NoteDetailsClient() {
         <div className={css.header}>
           <h2>{note.title}</h2>
         </div>
+
         <p className={css.tag}>{note.tag}</p>
         <p className={css.content}>{note.content}</p>
         <p className={css.date}>{note.createdAt}</p>
